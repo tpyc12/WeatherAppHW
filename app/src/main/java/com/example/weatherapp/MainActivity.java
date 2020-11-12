33d0122525a40958f7c32d78d7146f89e7cccea0
tpyc12 <tpyc12@yandex.ru>
2020-11-12 20:12:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String WEATHER_URL = "http://api.openweathermap.org/data/2.5/weather?q=%s&appid=6ae260b1fd6b6df0a257fff6e602c5bb&lang=ru&units=metric";
+
+    private EditText editTextCity;
+    private TextView textViewWeatherInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editTextCity = findViewById(R.id.editTextCity);
+        textViewWeatherInfo = findViewById(R.id.textViewWeatherInfo);
+    }
+
+    public void onClickShowWeather(View view) {
+        String city = editTextCity.getText().toString().trim();
+        if (!city.isEmpty()){
+            DownloadWeatherTask task = new DownloadWeatherTask();
+            String url = String.format(WEATHER_URL, city);
+            task.execute(url);
+        }
+    }
+
+    private class DownloadWeatherTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            StringBuilder result = new StringBuilder();
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while (line != null){
+                    result.append(line);
+                    line = reader.readLine();
+                }
+                return result.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null){
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String name = jsonObject.getString("name");
+                String temp = jsonObject.getJSONObject("main").getString("temp");
+                String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
+                String weather = String.format("%s\nТемпература: %s\nНа улице: %s", name, temp,description);
+                textViewWeatherInfo.setText(weather);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
